9854b1c1919f701c8d6f863aca095fd0581fbd1b
Patrick Neff <odie86@gmail.com>
2018-10-20 08:09:28 +0200

    updates

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -20,16 +20,16 @@ end
 
 function fish_prompt
   if test "$USER" != "$CURRENT_USER"
-      set user_prompt "$prompt$green$USER$normal$separator"
+      set -l user_prompt "$prompt$green$USER$normal$separator"
   end
 
   if test "$SSH_CLIENT" != ""
-      set host_prompt "$prompt$cyan$hostname$normal$separator"
+      set -l host_prompt "$prompt$cyan$hostname$normal$separator"
   end
 
-  set cwd_prompt "$cwd$normal"
+  set -l cwd_prompt "$cwd$normal"
 
-  set prompt "$user_prompt$host_prompt$cwd_prompt$normal "
+  set -l prompt "$user_prompt$host_prompt$cwd_prompt$normal "
 
   echo -n $prompt
 end
