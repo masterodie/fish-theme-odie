9541e93d26657541bd27e51d6ee46533764d121a
Patrick Neff <odie86@gmail.com>
2018-10-20 07:38:34 +0200

    updates

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,25 +1,37 @@
-# name: odie (based on mars)
+# name: odie
+
+set -l hostname (hostname | string replace -r '\..*' '')
+set -l separator '::'
+set -l last_status $status
+set -l cyan (set_color -o cyan)
+set -l yellow (set_color -o yellow)
+set -l red (set_color -o red)
+set -l blue (set_color -o blue)
+set -l green (set_color -o green)
+set -l magenta (set_color -o magenta)
+set -l normal (set_color normal)
+set -l cwd (prompt_pwd)
+
+if test $last_status = 0
+  set arrow " $green▸"
+else
+  set arrow " $red▸"
+end
 
 function fish_prompt
-  set -l hostname (hostname | string replace -r '\..*' '')
-  set -l separator '::'
-  set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -l magenta (set_color -o magenta)
-  set -l normal (set_color normal)
+  set -l prompt
 
-  if test $last_status = 0
-      set arrow " $green▸"
-  else
-      set arrow " $red▸"
+  if [ $USER -ne $CURRENT_USER ]
+      set -l prompt "$green$USER$normal$separator"
   end
-  set -l cwd $yellow(prompt_pwd)
 
-  echo -n -s "$green$USER$normal$separator$cyan$hostname$normal$separator$cwd$normal$arrow$normal "
+  if [ -z $SSH_CLIENT ]
+      set -l prompt "$prompt$cyan$hostname$normal$separator"
+  end
+
+  set -l "$prompt$yellow$cwd$normal$arrow$normal "
+
+  echo -n $prompt
 end
 
 
